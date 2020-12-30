2f6ec6e503cb8151d9d46255799692019c452551
nagasairaavi <nagasairaavi999@gmail.com>
2020-12-30 19:11:42 +0530

    humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast=(latitude,longitude,callback)=>{
         }
         else{
 
-            callback(undefined,body.current.weather_descriptions[0]+'. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out, humidity is '+body.current.humidity)
             
         }
     })
